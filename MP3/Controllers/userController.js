f90d32d6c1b1affd934379f22bab4c5e6d3be07d
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-06-26 20:30:11 +1200

    WIP

--- MP3/Controllers/userController.js
+++ MP3/Controllers/userController.js
@@ -22,6 +22,28 @@ const createUser = (data, res) => {
     }) 
 }
 
+const updateUser = (req, res) => {
+    // updates the user matching the ID from the param using JSON data POSTed in request body
+    console.log(req.body)
+    Models.User.findByIdAndUpdate(req.params.id, req.body, { 
+   new: true })
+        .then(data => res.send({result: 200, data: data}))
+        .catch(err => {
+             console.log(err);
+             res.send({result: 500, error: err.message})
+         }) 
+   }
+
+const deleteUser = (req, res) => {
+     // deletes the user matching the ID from the param
+    Models.User.findByIdAndDelete(req.params.id)
+        .then(data => res.send({result: 200, data: data}))
+        .catch(err => {
+            console.log(err);
+            res.send({result: 500, error: err.message})
+        }) 
+   }
+   
 module.exports = {
  getUsers, createUser
 }
