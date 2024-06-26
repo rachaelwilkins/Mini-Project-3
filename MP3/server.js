bdae053f849531c92258151877a4b81402a0b7b4
rachaelwilksin <rachaelelizabethwilkins@outlook.com>
2024-06-26 19:30:19 +1200

    WIP

--- MP3/server.js
+++ MP3/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+let dbConnect = require("./dbConnect")
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -12,5 +13,5 @@ app.get("/", (req, res) => {
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
- console.log(`Server is running on port ${PORT}.`);
+    console.log(`Server is running on port ${PORT}.`);
 });
